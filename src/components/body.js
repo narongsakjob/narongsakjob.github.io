b353dd88d62da0febdd10c48b183c79a9cbf9d1b
Narongsak <narongsak.chobsri@gmail.com>
2018-01-17 20:55:42 +0700

    Add bootstrap

---
+++ src/components/body.js
@@ -0,0 +1,7 @@
+import React from 'react'
+const Body = () => (
+	<div>Hello Body
+		<div href="#" className="btn btn-danger">asd</div>
+	</div>
+)
+export default Body
---
+++ src/components/header.js
@@ -0,0 +1,5 @@
+import React from 'react'
+const Header = () => (
+	<div>Hello world header</div>
+)
+export default Header
