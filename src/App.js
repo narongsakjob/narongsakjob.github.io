92f525a79e925224e7a5fa6b48b4210a831e4f49
Narongsak <narongsak.chobsri@gmail.com>
2018-01-18 02:08:24 +0700

    Add i18 n

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react'
+
+//i18n
+import { translate, Trans } from 'react-i18next'
+
+//Components
 import Header from './components/header'
 import Body from './components/body'
 import Footer from './components/footer'
+
 class App extends Component {
   render() {
+    const { t, i18n } = this.props;
+
+    const changeLanguage = (lng) => {
+      i18n.changeLanguage(lng);
+    }
+
     return (
       <div id="App">
-        <Header />
-        <Body />
-        <Footer />
-      </div>
+        <button onClick={() => changeLanguage('de')}>de</button>
+        <button onClick={() => changeLanguage('en')}>en</button>
+        <div className="App-intro">
+          <Trans i18nKey="description.part1">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </Trans>
+        </div>
+          <div>{t('description.part2')}</div>
+        </div>
     )
   }
 }
 
-export default App;
+export default translate('translations')(App);
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import './i18n';
 
 // styles
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
