7ca57ecbcb08abf84fb85c20df8c7ef6cbd7dc7b
Narongsak <narongsak.chobsri@gmail.com>
2018-01-23 13:49:13 +0700

    Change const to class

--- src/sections/body/project.js
+++ src/sections/body/project.js
@@ -9,25 +9,31 @@ const Image = styled.img`
 	height: 70%;
 `
 
-var table = []
-for (let j = 0; j < 5; j++) {
-	table.push(
-		<Container style={{ backgroundColor: `${ j%2===0 ? '': '#f3f3f3'}` }} key={j}>
-			<div className="container row">
-				<div class="col-md-6">
-					<h2>Heading</h2>
-					<p>Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Donec ullamcorper nulla non metus auctor fringilla.</p>
-				</div>
-				<div class="col-md-6">
-					<Image src={ require("../../assets/images/projects/wallet_1.png") } />
-				</div>
-			</div>
-		</Container>
-	)
+export default class Project extends React.Component {
+
+	table() {
+		var table = []
+		for (let j = 0; j < 5; j++) {
+			table.push(
+				<Container style={{ backgroundColor: `${ j%2===0 ? '': '#f3f3f3'}` }} key={j}>
+					<div className="container row">
+						<div class="col-md-6">
+							<h2>Heading</h2>
+							<p>Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Donec ullamcorper nulla non metus auctor fringilla.</p>
+						</div>
+						<div class="col-md-6">
+							<Image src={ require("../../assets/images/projects/wallet_1.png") } />
+						</div>
+					</div>
+				</Container>
+			)
+		}
+		return table
+	}
+
+	render() {
+		return (
+			this.table()
+		)
+	}
 }
-
-const Project = () => (
-	table
-)
-
-export default Project
