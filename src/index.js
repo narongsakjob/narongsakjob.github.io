3c7e16ceba15cbdabe732c62dffa2893f262440e
Narongsak <narongsak.chobsri@gmail.com>
2018-01-25 00:26:06 +0700

    Add react-slick for carousel

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import './i18n';
 // styles
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
