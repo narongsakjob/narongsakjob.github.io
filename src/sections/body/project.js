0cc69645565f7eaf59068b6aecf1ddd278024b7f
Narongsak <narongsak.chobsri@gmail.com>
2018-01-24 22:23:53 +0700

    Fix section image projects

--- src/sections/body/project.js
+++ src/sections/body/project.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
-	padding: 10%;
+	padding: 10% 10% 0% 10%;
 `
 const Image = styled.img`
 	width: 60%;
@@ -26,9 +26,9 @@ export default class Project extends React.Component {
 		)
 	}
 
-	getImage() {
+	getImage(index) {
 		return(
-			<div class="col-md-6">
+			<div class="col-md-6 text-center">
 				<Image src={ require("../../assets/images/projects/wallet_1.png") } />
 			</div>
 		)
@@ -42,8 +42,8 @@ export default class Project extends React.Component {
 			table.push(
 				<Container style={{ backgroundColor: `${ j%2===0 ? '': '#f3f3f3'}` }} key={j}>
 					<div className="container row">
-						{ j%2 === 0 ? this.getData(j) : this.getImage() }
-						{ j%2 !== 0 ? this.getData(j) : this.getImage() }
+						{ j%2 === 0 ? this.getData(j) : this.getImage(j) }
+						{ j%2 !== 0 ? this.getData(j) : this.getImage(j) }
 					</div>
 				</Container>
 			)
