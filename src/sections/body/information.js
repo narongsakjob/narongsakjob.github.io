b0b8c391cd2de91df37312f564c9762b1661bdea
Narongsak <narongsak.chobsri@gmail.com>
2018-01-26 16:53:42 +0700

    Modify container information

--- src/sections/body/information.js
+++ src/sections/body/information.js
@@ -7,20 +7,26 @@ const Information = styled.div`
 	box-shadow: 2px 2px 14px rgba(0, 0, 0, 0.25);
 	background-color: #ede3e1;
 	padding: 20px 0 20px 0;
+	margin: 0 auto 0 auto;
+	font-weight: bold;
+	width: 60%;
 `
+const Label = styled.label`
+	font-weight: bold;
+`
+
 const Profile = ( {t} ) => (
 	<div>
 		<Information className="hidden-md-down">
-			<div className="row"><label className="col-md-5 text-right">{ t('about.first_name.label') } : </label> <div className="col-md-3">{ t('about.first_name.value') } { t('about.last_name.value') }</div></div>
-			<div className="row"><label className="col-md-5 text-right">{ t('about.nickname.label') } : </label> <div className="col-md-3">{ t('about.nickname.value') }</div></div>
-			<div className="row"><label className="col-md-5 text-right">{ t('about.birthdate.label') } : </label> <div className="col-md-3">{ t('about.birthdate.value') }</div></div>
-			<div className="row"><label className="col-md-5 text-right">{ t('about.age.label') } : </label> <div className="col-md-3">{ t('about.age.value') } { t('about.age.unit') }</div></div>
-			<div className="row"><label className="col-md-5 text-right">{ t('about.nationality.label') } : </label> <div className="col-md-3">{ t('about.nationality.value') }</div></div>
-			<div className="row"><label className="col-md-5 text-right">{ t('about.education.label') } : </label> <div className="col-md-4">{ t('about.education.value') } <label>{ t('about.major.label') }</label></div></div>
-			<div className="row"><div className="col-md-5"/><div className="col-md-4">&nbsp;{ t('about.major.value') }</div></div>
+			<div className="row"><label className="col-md-4 text-right">{ t('about.first_name.label') } : </label> <div className="col-md-8 text-left">{ t('about.first_name.value') } { t('about.last_name.value') }</div></div>
+			<div className="row"><label className="col-md-4 text-right">{ t('about.nickname.label') } : </label> <div className="col-md-8 text-left">{ t('about.nickname.value') }</div></div>
+			<div className="row"><label className="col-md-4 text-right">{ t('about.birthdate.label') } : </label> <div className="col-md-8 text-left">{ t('about.birthdate.value') }</div></div>
+			<div className="row"><label className="col-md-4 text-right">{ t('about.age.label') } : </label> <div className="col-md-8 text-left">{ t('about.age.value') } { t('about.age.unit') }</div></div>
+			<div className="row"><label className="col-md-4 text-right">{ t('about.nationality.label') } : </label> <div className="col-md-8 text-left">{ t('about.nationality.value') }</div></div>
+			<div className="row"><label className="col-md-4 text-right">{ t('about.education.label') } : </label> <div className="col-md-8 text-left">{ t('about.education.value') } <label>{ t('about.major.label') }</label> { t('about.major.value') }</div></div>
 		</Information>
 		<Information className="hidden-lg-up">
-			<div><label>{ t('about.first_name.label') } : </label> { t('about.first_name.value') } { t('about.last_name.value') } </div>
+			<div><Label>{ t('about.first_name.label') } : </Label> { t('about.first_name.value') } { t('about.last_name.value') } </div>
 			<div><label>{ t('about.nickname.label') } : </label> { t('about.nickname.value') }</div>
 			<div><label>{ t('about.birthdate.label') } : </label> { t('about.birthdate.value') }</div>
 			<div><label>{ t('about.age.label') } : </label> { t('about.age.value') } { t('about.age.unit') }</div>
