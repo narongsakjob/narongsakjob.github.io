c4c225fd375be1f34dd3d92688f4d0ef526064a7
Narongsak <narongsak.chobsri@gmail.com>
2018-01-26 15:52:15 +0700

    Add contact style

--- src/sections/body/contact.js
+++ src/sections/body/contact.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const Title = styled.div`
+`
+
 const Container = styled.div`
 	margin-top: 20px;
 	padding: 20px 0 20px 0;
@@ -9,23 +12,35 @@ const Address = styled.div`
 	border-radius: 10px;
 	box-shadow: 2px 2px 14px rgba(0, 0, 0, 0.25);
 	background-color: #ede3e1;
+	align-self: center;
+	padding: 20px 0 20px 0;
+`
+const Label = styled.button`
+	border: 0;
+	padding: 5px 20px 5px 20px;
+	border-radius: 20px;
+	margin-bottom: 10px;
+	&:hover {
+		cursor: pointer;
+		
+	}
 `
 
 const contact = ({t}) => (
 	<Container className="row">
 		<div className="col-md-6">
 			<div className="row">
-				<a className="col-md-6 text-right" href="https://www.facebook.com/NarongsakJob"><i className="fa fa-facebook-official" aria-hidden="true"></i> Facebook</a>
-				<a className="col-md-6 text-center" href="https://github.com/narongsakjob"><i className="fa fa-github" aria-hidden="true"></i> Github</a>
+				<a className="col-md-6 text-right" href="https://www.facebook.com/NarongsakJob" target="_blank"><Label><i className="fa fa-facebook-official" aria-hidden="true"></i> Facebook</Label></a>
+				<a className="col-md-6 text-center" href="https://github.com/narongsakjob" target="_blank"><Label><i className="fa fa-github" aria-hidden="true"></i> Github</Label></a>
 			</div>
 			<div className="row">			
-				<a className="col-md-6 text-right" href="http://line.me/ti/p/~job_ynwa"><i className="fa fa-comment-o" aria-hidden="true"></i> Line</a>
-				<a className="col-md-6" href="mailto:narongsak.chobsri@gmail.com"><i className="fa fa-envelope" aria-hidden="true"></i> Email</a>
+				<a className="col-md-6 text-right" href="http://line.me/ti/p/~job_ynwa" target="_blank"><Label><i className="fa fa-comment-o" aria-hidden="true"></i> Line</Label></a>
+				<a className="col-md-6" href="mailto:narongsak.chobsri@gmail.com" target="_blank"><Label><i className="fa fa-envelope" aria-hidden="true"></i> Email</Label></a>
 			</div>
-			<div><i className="fa fa-phone" aria-hidden="true"></i>+66 911018404</div>
+			<div className="row"><div className="col-md-4"/><Label className="col-md-6"><i className="fa fa-phone" aria-hidden="true"></i> +66 911018404</Label></div>
 		</div>
 		<Address className="col-md-4">
-			<div>Address</div>
+			<h3>Address</h3>
 			{t('about.address')}
 		</Address>
 	</Container>
