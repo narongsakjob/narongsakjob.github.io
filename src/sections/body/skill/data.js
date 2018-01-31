e879388ee0c87ab16e7693df32a6f1483ea3ba75
Narongsak <narongsak.chobsri@gmail.com>
2018-01-31 22:24:20 +0700

    Update status skill

--- src/sections/body/skill/data.js
+++ src/sections/body/skill/data.js
@@ -2,56 +2,56 @@ const data = [
 	{
 		"name": "Ruby",
 		"image": "ruby",
-		"percent": "70",
+		"percent": "60",
 		"framework": "Ruby on Rails",
 		"color": "#D62D20" 		
 	},
 	{
 		"name": "Python",
 		"image": "python",
-		"percent": "65",
+		"percent": "55",
 		"framework": "Django",
 		"color": "#6897BB"	
 	},
 	{
 		"name": "PHP",
 		"image": "php",
-		"percent": "45",
+		"percent": "30",
 		"framework": "Laravel",
 		"color": "#468499"
 	},
 	{
 		"name": "Javascript",
 		"image": "javascript",
-		"percent": "55",
+		"percent": "45",
 		"framework": "jQuery, React, Vue.js, Node.js",
 		"color": "#FFD700"
 	},
 	{
 		"name": "HTML+CSS",
 		"image": "html",
-		"percent": "70",
+		"percent": "60",
 		"framework": "Slim, Bootstrap, SASS",
 		"color": "#FF8000"
 	},
 	{
 		"name": "Java",
 		"image": "java",
-		"percent": "50",
+		"percent": "40",
 		"framework": "Andriod",
 		"color": "#ec9b00"
 	},
 	{
 		"name": "Swift",
 		"image": "swift",
-		"percent": "30",
+		"percent": "20",
 		"framework": "iOS",
 		"color": "#FFA500"
 	},
 	{
 		"name": "C/C++",
 		"image": "c",
-		"percent": "35",
+		"percent": "25",
 		"framework": "Data Structure",
 		"color": "#C0D6E4"
 	}
